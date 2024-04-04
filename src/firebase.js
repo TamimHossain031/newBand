136d9529fb0185dd560711f87f8eb70fba520a7a
Tamim <tamimhossain031@gmail.com>
2024-04-04 21:17:00 +0600

    all

--- src/firebase.js
+++ src/firebase.js
@@ -9,19 +9,17 @@ import {
 } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDGzNURAT4jLhKpS8iC6wH-V8geE8wHLrI",
+  apiKey: `${import.meta.env.VITE_API_KEY}`,
+  authDomain: "fake-b46ec.firebaseapp.com",
 
-  authDomain: "hosting-87370.firebaseapp.com",
+  projectId: "fake-b46ec",
 
-  projectId: "hosting-87370",
+  storageBucket: "fake-b46ec.appspot.com",
 
-  storageBucket: "hosting-87370.appspot.com",
+  messagingSenderId: "353389317122",
 
-  messagingSenderId: "809920948971",
+  appId: "1:353389317122:web:6eb318aa2501973f18eb1b"
 
-  appId: "1:809920948971:web:962cc280444a3332b36cae",
-
-  measurementId: "G-J339GPV66B",
 };
 
 const app = initializeApp(firebaseConfig);
